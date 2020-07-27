95180ea5aa60c2850ea3f4d222eec442cc85cff5
davinegreiiros <davi.negreiiros36@gmail.com>
2020-07-27 11:55:25 -0300

    Bad solution

--- src/model/entities/Reservertion.java
+++ src/model/entities/Reservertion.java
@@ -40,15 +40,30 @@ public class Reservertion {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
 	public String toString() {
-		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) 
-		+ ", check-out: " + sdf.format(checkOut) + ", " + duration() + " nights";
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
 	}
 	
 }
